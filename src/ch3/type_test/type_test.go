0b5295547ff41b2d6453d1c821c3172ac2457249
zhaozhonghao <zhonghao@xiaoe-tech.com>
2020-12-04 16:02:33 +0800

    string 类型初始化为空字符串，不是nil

--- src/ch3/type_test/type_test.go
+++ src/ch3/type_test/type_test.go
@@ -25,8 +25,17 @@ func TestImplicit(t *testing.T) {
 func TestPoint(t *testing.T) {
 	a := 1
 	aPtr := &a
-	aPtr = aPtr + 1
+	// aPtr = aPtr + 1
 	t.Log(a, aPtr)
 	t.Logf("%T %T", a, aPtr)
 
+}
+
+func TestString(t *testing.T) {
+	var s string
+	t.Log("*" + s + "*")
+	t.Log(len(s))
+	if s == "" {
+
+	}
 }
