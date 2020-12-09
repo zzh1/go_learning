183a88050fbd8af759cbb1d91e5b1ef06905126f
zhaozhonghao <zhonghao@xiaoe-tech.com>
2020-12-09 17:57:20 +0800

    数组便利

--- src/ch6/array_test/array_test.go
+++ src/ch6/array_test/array_test.go
@@ -12,3 +12,17 @@ func TestArrayInit(t *testing.T) {
 	t.Log(arr[1], arr[2])
 	t.Log(arr1, arr3)
 }
+
+func TestArrayTravel(t *testing.T) {
+	arr3 := [...]int{1, 3, 4, 5}
+	for i := 0; i < len(arr3); i++ {
+		t.Log(arr3[i])
+	}
+	for idx, e := range arr3 {
+		t.Log(idx, e)
+	}
+	for _, e := range arr3 {
+		t.Log(e)
+	}
+
+}
