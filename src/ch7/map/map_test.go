aedf6a269860bd303700030dd134f58bcaed1790
zhaozhonghao <zhonghao@xiaoe-tech.com>
2020-12-10 16:23:51 +0800

    map值不存在时的情况

--- src/ch7/map/map_test.go
+++ src/ch7/map/map_test.go
@@ -14,3 +14,20 @@ func TestInitMap(t *testing.T) {
 	t.Logf("len m3=%d", len(m3))
 	// t.Logf("len m3=%d", cap(m3))
 }
+
+func TestAccessNotExistingKey(t *testing.T) {
+	m1 := map[int]int{}
+	t.Log(m1[1])
+	m1[2] = 0
+	t.Log(m1[2])
+	m1[3] = 0
+	//会返回两个变量，一个为值，一个为布尔。布尔为true，值存在；
+	//在访问的key不存在是，仍然返回零值，不能通过返回nil来判断元素是否存在
+	if v, ok := m1[3]; ok {
+		t.Logf("Key 3's value is %d", v)
+		t.Log(ok)
+	} else {
+		t.Log("Key 3 is not existing.")
+	}
+
+}
