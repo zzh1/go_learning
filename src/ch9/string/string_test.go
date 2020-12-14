3f73107872de768acf0ce0f2b6be1db78715fcb1
zhaozhonghao <zhonghao@xiaoe-tech.com>
2020-12-14 17:46:28 +0800

    字符串编码

--- src/ch9/string/string_test.go
+++ src/ch9/string/string_test.go
@@ -14,4 +14,13 @@ func TestString(t *testing.T) {
 	// s = "\xE4\xBA\xBB\xFF" //可以存储任何二进制数据
 	t.Log(s)
 	t.Log(len(s))
+
+	s = "中"
+	t.Log(len(s)) //是byte数
+
+	c := []rune(s)
+	t.Log(len(c))
+	t.Logf("中 unicode %x", c[0])
+	t.Logf("中 UTF8 %x", s)
+
 }
