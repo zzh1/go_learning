bd9e26eeae5bf6dac0eea61e02649e391cacf029
zhaozhonghao <zhonghao@xiaoe-tech.com>
2020-12-15 11:02:28 +0800

    测试函数时间

--- src/ch10/func/func_test.go
+++ src/ch10/func/func_test.go
@@ -1,17 +1,35 @@
 package fn_test
 
 import (
+	"fmt"
 	"math/rand"
 	"testing"
+	"time"
 )
 
 func returnMultiValues() (int, int) {
 	return rand.Intn(10), rand.Intn(20)
 }
 
+func timeSpent(inner func(op int) int) func(op int) int {
+	return func(n int) int {
+		start := time.Now()
+		ret := inner(n)
+		fmt.Println("time spent:", time.Since(start).Seconds())
+		return ret
+	}
+}
+
+func slowFun(op int) int {
+	time.Sleep(time.Second * 1)
+	return op
+}
+
 func TestFn(t *testing.T) {
 	// a, b := returnMultiValues()
 	//不需要某个参数，可以使用占位符
-	a, _ := returnMultiValues()
-	t.Log(a)
+	// a, _ := returnMultiValues()
+	// t.Log(a)
+	tsSF := timeSpent(slowFun)
+	t.Log(tsSF(10))
 }
