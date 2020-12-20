29677d20f670017e865fdbb5811f0b3262772875
zhaozhonghao <zhonghao@xiaoe-tech.com>
2020-12-20 15:49:14 +0800

    扩展

---
+++ src/ch12/extension/extension_test.go
@@ -0,0 +1,35 @@
+package extension
+
+import (
+	"fmt"
+	"testing"
+)
+
+type Pet struct {
+}
+
+func (p *Pet) Speak() {
+	fmt.Print("...")
+}
+
+func (p *Pet) SpeakTo(host string) {
+	p.Speak()
+	fmt.Println(" ", host)
+}
+
+type Dog struct {
+	p *Pet
+}
+
+func (d *Dog) Speak() {
+	d.p.Speak()
+}
+
+func (d *Dog) SpeakTo(host string) {
+	d.p.SpeakTo(host)
+}
+
+func TestDog(t *testing.T) {
+	dog := new(Dog)
+	dog.SpeakTo("chao")
+}
