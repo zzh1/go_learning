0dcbde79027f6e4f59b78f8fd8cc0d90f650f344
zhaozhonghao <zhonghao@xiaoe-tech.com>
2020-12-20 15:52:54 +0800

    符合

--- src/ch12/extension/extension_test.go
+++ src/ch12/extension/extension_test.go
@@ -22,11 +22,16 @@ type Dog struct {
 }
 
 func (d *Dog) Speak() {
-	d.p.Speak()
+	// d.p.Speak()
+
+	fmt.Println("Wang!")
 }
 
 func (d *Dog) SpeakTo(host string) {
-	d.p.SpeakTo(host)
+	// d.p.SpeakTo(host)
+
+	d.Speak()
+	fmt.Println(" ", host)
 }
 
 func TestDog(t *testing.T) {
