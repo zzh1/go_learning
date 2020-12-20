9b1dfa4dfb90e5990103c49b2968718488950ea3
zhaozhonghao <zhonghao@xiaoe-tech.com>
2020-12-20 17:35:55 +0800

    swith语句改写

--- src/ch13/empty_interface/empty_interface_test.go
+++ src/ch13/empty_interface/empty_interface_test.go
@@ -7,17 +7,25 @@ import (
 
 func DoSomething(p interface{}) {
 	//断言的方式， p.()   即p如果是int类型
-	if i, ok := p.(int); ok {
-		fmt.Println("Interger", i)
-		return
-	}
+	// if i, ok := p.(int); ok {
+	// 	fmt.Println("Interger", i)
+	// 	return
+	// }
+	// if s, ok := p.(string); ok {
+	// 	fmt.Println("string", s)
+	// 	return
+	// }
+	// fmt.Println("Unknow Type")
 
-	if s, ok := p.(string); ok {
-		fmt.Println("string", s)
-		return
+	//使用switch方式
+	switch v := p.(type) {
+	case int:
+		fmt.Println("Interger", v)
+	case string:
+		fmt.Println("String", v)
+	default:
+		fmt.Println("Unknow Type")
 	}
-
-	fmt.Println("Unknow Type")
 }
 
 func TestEmptyInterfaceAssertion(t *testing.T) {
