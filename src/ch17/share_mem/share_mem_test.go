dd9d8d75c17a8b7e94df7a3f968685e03851c99c
zhaozhonghao <zhonghao@xiaoe-tech.com>
2020-12-24 16:36:46 +0800

    加锁的计数器

--- src/ch17/share_mem/share_mem_test.go
+++ src/ch17/share_mem/share_mem_test.go
@@ -1,6 +1,7 @@
 package share_mem
 
 import (
+	"sync"
 	"testing"
 	"time"
 )
@@ -9,6 +10,22 @@ func TestCounter(t *testing.T) {
 	counter := 0
 	for i := 0; i < 5000; i++ {
 		go func() {
+			counter++
+		}()
+	}
+	time.Sleep(1 * time.Second)
+	t.Logf("counter=%d", counter)
+}
+
+func TestCounterThreadSafe(t *testing.T) {
+	var mut sync.Mutex
+	counter := 0
+	for i := 0; i < 5000; i++ {
+		go func() {
+			defer func() {
+				mut.Unlock()
+			}()
+			mut.Lock()
 			counter++
 		}()
 	}
