89372c86e504005ee9f4340f4702d0cc6f9387b5
zhaozhonghao <zhonghao@xiaoe-tech.com>
2020-12-24 15:24:07 +0800

    未加锁的，线程不安全的

---
+++ src/ch17/share_mem/share_mem_test.go
@@ -0,0 +1,17 @@
+package share_mem
+
+import (
+	"testing"
+	"time"
+)
+
+func TestCounter(t *testing.T) {
+	counter := 0
+	for i := 0; i < 5000; i++ {
+		go func() {
+			counter++
+		}()
+	}
+	time.Sleep(1 * time.Second)
+	t.Logf("counter=%d", counter)
+}
