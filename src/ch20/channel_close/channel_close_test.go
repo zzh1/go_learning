37328136d63ee912368e16319fc68eab6ddffcfa
zhaozhonghao <zhonghao@xiaoe-tech.com>
2020-12-27 11:43:28 +0800

    生产者-消费者。规定好10个数

---
+++ src/ch20/channel_close/channel_close_test.go
@@ -0,0 +1,36 @@
+package channel_close
+
+import (
+	"fmt"
+	"sync"
+	"testing"
+)
+
+func dataProducer(ch chan int, wg *sync.WaitGroup) {
+	go func() {
+		for i := 0; i < 10; i++ {
+			ch <- i
+		}
+		wg.Done()
+	}()
+}
+
+func dataReceiver(ch chan int, wg *sync.WaitGroup) {
+	go func() {
+		for i := 0; i < 10; i++ {
+			data := <-ch
+			fmt.Println(data)
+		}
+		wg.Done()
+	}()
+}
+
+func TestCloseChannel(t *testing.T) {
+	var wg sync.WaitGroup
+	ch := make(chan int)
+	wg.Add(1)
+	dataProducer(ch, &wg)
+	wg.Add(1)
+	dataReceiver(ch, &wg)
+	wg.Wait()
+}
