d82d1e691b2f57519323ebc70723810e0a4049ce
zhaozhonghao <zhonghao@xiaoe-tech.com>
2020-12-27 11:54:16 +0800

    使用channel关闭机制，无须再做特殊规定。 也可以使用多个receiver

--- src/ch20/channel_close/channel_close_test.go
+++ src/ch20/channel_close/channel_close_test.go
@@ -11,15 +11,21 @@ func dataProducer(ch chan int, wg *sync.WaitGroup) {
 		for i := 0; i < 10; i++ {
 			ch <- i
 		}
+		//关闭channel
+		close(ch)
 		wg.Done()
 	}()
 }
 
 func dataReceiver(ch chan int, wg *sync.WaitGroup) {
 	go func() {
-		for i := 0; i < 10; i++ {
-			data := <-ch
-			fmt.Println(data)
+		for {
+			// ok会返回channel状态，false的话，即channel关闭。无须再和生产者规定10
+			if data, ok := <-ch; ok {
+				fmt.Println(data)
+			} else {
+				break
+			}
 		}
 		wg.Done()
 	}()
@@ -30,6 +36,9 @@ func TestCloseChannel(t *testing.T) {
 	ch := make(chan int)
 	wg.Add(1)
 	dataProducer(ch, &wg)
+	wg.Add(1)
+	dataReceiver(ch, &wg)
+	//可以采用多个receiver
 	wg.Add(1)
 	dataReceiver(ch, &wg)
 	wg.Wait()
