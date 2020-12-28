ae9547909f74301188e7b4a64e3b1508e0f347b7
zhaozhonghao <zhonghao@xiaoe-tech.com>
2020-12-28 16:57:27 +0800

    使用csp模式实现 '全部协程完成'

---
+++ src/ch23/until_all_done/until_all_done_test.go
@@ -0,0 +1,38 @@
+package until_all_done
+
+import (
+	"fmt"
+	"runtime"
+	"testing"
+	"time"
+)
+
+func runTask(id int) string {
+	time.Sleep(10 * time.Millisecond)
+	return fmt.Sprintf("The result is from %d", id)
+}
+
+func AllResponse() string {
+	numOfRunner := 10
+	// ch := make(chan string)
+	//使用buffered channel。消息的产生者不需要等待消息的接收者把消息拿走，消息产生者和接收者解藕
+	ch := make(chan string, numOfRunner)
+	for i := 0; i < numOfRunner; i++ {
+		go func(i int) {
+			ret := runTask(i)
+			ch <- ret
+		}(i)
+	}
+	finalRet := ""
+	for j := 0; j < numOfRunner; j++ {
+		finalRet += <-ch + "\n"
+	}
+	return finalRet
+}
+
+func TestFirstResponse(t *testing.T) {
+	t.Log("Before:", runtime.NumGoroutine())
+	t.Log(AllResponse())
+	time.Sleep(time.Second * 1)
+	t.Log("After:", runtime.NumGoroutine())
+}
